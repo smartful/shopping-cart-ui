13b5b24d0122baaaf2ce88e2b3dd9cc0d93a9a0a
smartful <remi_rodrigues@hotmail.com>
2025-08-21 06:06:51 +0200

    add Cart feature

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useContext, useState } from 'react';
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart((prevState) => {
+      const existing = prevState.find((item) => item.id === product.id);
+      if (existing) {
+        return prevState.map((item) => (item.id === product.id ? { ...item, qty: item.qty + 1 } : item));
+      }
+
+      return [...prevState, { ...product, qty: 1 }];
+    });
+  };
+
+  return <CartContext.Provider value={{ cart, addToCart }}>{children}</CartContext.Provider>;
+}
+
+export function useCart() {
+  return useContext(CartContext);
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { CartProvider } from './context/CartContext';
 import { ProductProvider } from './context/ProductContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ProductProvider>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </ProductProvider>
   </StrictMode>
 );
