c0a343f3982c6da9ab03b8bb915b31b8abd2389f
smartful <remi_rodrigues@hotmail.com>
2025-08-21 05:18:25 +0200

    use of the React Context API

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,8 +1,14 @@
+import { useContext } from 'react';
 import ProductCard from './ProductCard';
+import { ProductContext } from '../context/ProductContext';
 
-const ProductList = ({ products }) => {
+const ProductList = () => {
+  const { products, loading, error } = useContext(ProductContext);
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      {loading && <p>Loading ...</p>}
+      {error && <div className="error">❌ {error}</div>}
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
